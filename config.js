f231262414e88147bb618435098de60d3f3141bd
Flávio Pereira <github@flaviopereira.dev>
2024-09-28 01:25:16 +0100

    created commands\github.js
    provides urls for github repos

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
     INVITE_URL                : "http://discord.hondatabase.com",
     STAFF_CHANNEL_ID          : process.env.STAFF_CHANNEL_ID,
     HANGOUT_CHANNEL_ID        : process.env.HANGOUT_CHANNEL_ID,
-    ARTICLE_REQUEST_CHANNEL_ID: process.env.ARTICLE_REQUEST_CHANNEL_ID
+    ARTICLE_REQUEST_CHANNEL_ID: process.env.ARTICLE_REQUEST_CHANNEL_ID,
+    GITHUB_ORG_URL            : "https://github.com/hondatabase/"
 };
